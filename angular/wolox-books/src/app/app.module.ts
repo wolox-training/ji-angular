b515397a67413599eab7f7a80d1969b85fc1b803
Juan Camilo Isaza <juancamilo.isaza@wolox.co>
2018-07-13 16:59:39 -0500

    Adding register screen component

--- angular/wolox-books/src/app/app.module.ts
+++ angular/wolox-books/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './screens/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/wolox-books/src/app/screens/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent {
+
+  registerForm: FormGroup;
+  fieldRequired = 'This field is required';
+  validEmail = 'Enter a valid email';
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = fb.group({
+      'firstName': [null, Validators.required],
+      'lastName': [null, Validators.required],
+      'email': [null, Validators.compose([Validators.email, Validators.required])],
+      'password': [null, Validators.compose([Validators.minLength(6), Validators.required])]
+    });
+  }
+
+  register(data) {
+    const registration = `
+    {
+      "user": {
+        "email": ${data.email},
+        "password": ${data.password},
+        "password_confirmation": ${data.password},
+        "first_name": ${data.firstName},
+        "last_name": ${data.lastName},
+        "locale": "en"
+      }
+    }`;
+    console.log(registration);
+  }
+
+}
