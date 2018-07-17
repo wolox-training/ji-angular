3be9c072c99ba725fbdbb1682b954257c0003ff0
jkmaswell <jkmisaza@gmail.com>
2018-07-17 14:27:32 -0500

    Organizing app module

--- angular/wolox-books/src/app/app.module.ts
+++ angular/wolox-books/src/app/app.module.ts
@@ -7,10 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './screens/register/register.component';
 import { LoginComponent } from './screens/login/login.component';
+import { AuthComponent } from './components/common/auth/auth.component';
 
 import { UserService } from './components/services/user/user.service';
 import { LocalStorageService } from './components/services/local-storage/local-storage.service';
-import { AuthComponent } from './components/common/auth/auth.component';
 
 @NgModule({
   declarations: [
