6bc1747f4465a1eb753d0940199ba29216064c25
jkmaswell <jkmisaza@gmail.com>
2018-07-18 16:11:59 -0500

    Folder re-estructuring and path fixings

--- angular/wolox-books/src/app/app.module.ts
+++ angular/wolox-books/src/app/app.module.ts
@@ -6,16 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { UnauthComponent } from './screens/unauth/unauth.component';
-import { BookListComponent } from './screens/book-list/book-list.component';
-import { RegisterComponent } from './screens/unauth/register/register.component';
-import { LoginComponent } from './screens/unauth/login/login.component';
-import { BooksComponent } from './screens/book-list/books/books.component';
+import { AuthComponent } from './screens/auth/auth.component';
+import { RegisterComponent } from './screens/unauth/screens/register/register.component';
+import { LoginComponent } from './screens/unauth/screens/login/login.component';
+import { BooksComponent } from './screens/auth/screens/books/books.component';
 
-import { UserService } from './components/services/user/user.service';
-import { LocalStorageService } from './components/services/local-storage/local-storage.service';
-import { AuthService } from './components/services/auth/auth.service';
-import { AuthGuard } from './auth.guard';
-import { UnauthGuard } from './unauth.guard';
+import { UserService } from './services/user/user.service';
+import { LocalStorageService } from './services/local-storage/local-storage.service';
+import { AuthService } from './services/auth/auth.service';
+import { AuthGuard } from './guards/auth.guard';
+import { UnauthGuard } from './guards/unauth.guard';
 
 @NgModule({
   declarations: [
@@ -24,7 +24,7 @@ import { UnauthGuard } from './unauth.guard';
     LoginComponent,
     BooksComponent,
     UnauthComponent,
-    BookListComponent
+    AuthComponent
   ],
   imports: [
     BrowserModule,
