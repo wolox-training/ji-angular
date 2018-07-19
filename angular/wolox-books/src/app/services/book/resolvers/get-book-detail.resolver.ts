4f5375d8c5fd25acdeadbdc570a6a6246efddeb4
jkmaswell <jkmisaza@gmail.com>
2018-07-19 16:28:24 -0500

    Adding error handler

--- angular/wolox-books/src/app/services/book/resolvers/get-book-detail.resolver.ts
+++ angular/wolox-books/src/app/services/book/resolvers/get-book-detail.resolver.ts
@@ -1,15 +1,32 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
 
 import { BookService } from './../../../services/book/book.service';
 import { Book } from './../../../models/book.model';
 
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/of';
+
 @Injectable()
 export class GetBookDetailResolver implements Resolve<Book> {
-  constructor(private bookService: BookService) {}
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {}
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Book> {
-    return this.bookService.getBookDetail(route.params.id);
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Book> {
+    return this.bookService.getBookDetail(route.params.id).catch(error => {
+      this.router.navigate(['book-list']);
+      return Observable.of('data not available at this time');
+    });
   }
 }
