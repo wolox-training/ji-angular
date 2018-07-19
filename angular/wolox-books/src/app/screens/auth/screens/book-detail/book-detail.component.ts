9e8182ca30ad0bd407294c0dc417df64432c4586
jkmaswell <jkmisaza@gmail.com>
2018-07-19 12:16:51 -0500

    Adding trailing spaces

--- angular/wolox-books/src/app/screens/auth/screens/book-detail/book-detail.component.ts
+++ angular/wolox-books/src/app/screens/auth/screens/book-detail/book-detail.component.ts
@@ -9,6 +9,7 @@ import { Book } from './../../../../models/book.model';
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.scss']
 })
+
 export class BookDetailComponent implements OnInit {
   detail: Book;
 
