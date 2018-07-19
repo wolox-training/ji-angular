b6f5a0e33abfe7c44f8463ebe2c42f6a8f9d2da7
jkmaswell <jkmisaza@gmail.com>
2018-07-19 13:50:58 -0500

    Fixing branch

--- angular/wolox-books/src/app/constants/app-routes.constant.ts
+++ angular/wolox-books/src/app/constants/app-routes.constant.ts
@@ -5,6 +5,7 @@ import { AuthComponent } from '../screens/auth/auth.component';
 import { RegisterComponent } from '../screens/unauth/screens/register/register.component';
 import { LoginComponent } from '../screens/unauth/screens/login/login.component';
 import { BooksComponent } from '../screens/auth/screens/books/books.component';
+import { BookDetailComponent } from './../screens/auth/screens/book-detail/book-detail.component';
 
 import { AuthGuard } from '../guards/auth.guard';
 import { UnauthGuard } from '../guards/unauth.guard';
@@ -38,6 +39,10 @@ export const routes: Routes = [
       {
         path: '',
         component: BooksComponent,
+      },
+      {
+        path: ':id',
+        component: BookDetailComponent
       }
     ]
   },
